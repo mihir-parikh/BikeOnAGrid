2fb3bb1a79e89c328e0a8bcdb7e53a982997a302
Mihir Parikh <mparikh@genixventures.com>
2021-10-16 16:23:08 +1100

    v0.2 of Validation & place method

--- BikeOnAGrid.php
+++ BikeOnAGrid.php
@@ -6,14 +6,26 @@ class BikeOnAGrid {
         if(!isset($command[1])) {
             exit("Error: No command is passed as an argument.\n");
         }
-        else if(strpos($command[1], 'PLACE') === FALSE && $command[1] !== 'FORWARD' && $command[1] !== 'TURN_LEFT' 
+        else if($command[1] !== 'PLACE' && $command[1] !== 'FORWARD' && $command[1] !== 'TURN_LEFT' 
         && $command[1] !== 'TURN_RIGHT' && $command[1] !== 'GPS_REPORT') {
             exit("Error: Please pass a valid command from PLACE, FORWARD, TURN_LEFT, TURN_RIGHT, GPS_REPORT.\n");
         }
-        else {
-            // A valid command
-            return true;
+        else if($command[1] === 'PLACE') {
+            // If the arguments for PLACE command do not exist
+            if(!isset($command[2])) {
+                exit("Error: Valid arguments in the form of X,Y,DIRECTION is not passed for the command PLACE.\n");
+            }
         }
+        
+        // A valid command
+        return true;
+    }
+
+    public function place($x, $y, $direction) {
+        // FIXME: Temporary assumption: The passed x & y are within the boundary
+        $this->currentX = $x;
+        $this->currentY = $y;
+        $this->currentDirection = $direction;
     }
 }
 ?>
