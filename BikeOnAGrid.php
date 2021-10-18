ea45316a8c5f771ee4dae4fb57139eefde79ca5e
Mihir Parikh <mparikh@genixventures.com>
2021-10-18 17:58:51 +1100

    Minor refactoring: defining ERROR constants

--- BikeOnAGrid.php
+++ BikeOnAGrid.php
@@ -2,6 +2,10 @@
 class BikeOnAGrid {
     private $currentX, $currentY, $currentDirection;
 
+    const ERR_INVALID_COMMAND = "Error: Please pass a valid command from FORWARD, TURN_LEFT, TURN_RIGHT or GPS_REPORT for index 2+.\n";
+    const ERR_OOB_PLACEMENT = "Error: Bike placement must be within the 7 x 7 grid boundaries - i.e. between 0 to 6\n";
+    const ERR_OOB_FORWARD = "Error: Bike cannot move any further/out of 7 x 7 grid\n";
+
     public function __get($property) {
         if (property_exists($this, $property)) {
             return $this->$property;
@@ -17,7 +21,7 @@ class BikeOnAGrid {
 
     public function isValidCommand($command) {
         if($command !== 'FORWARD' && $command !== 'TURN_LEFT' && $command !== 'TURN_RIGHT' && $command !== 'GPS_REPORT') {
-            exit("Error: Please pass a valid command from FORWARD, TURN_LEFT, TURN_RIGHT or GPS_REPORT for index 2+.\n");
+            exit(BikeOnAGrid::ERR_INVALID_COMMAND);
         }
         
         // A valid command
@@ -27,7 +31,7 @@ class BikeOnAGrid {
     public function place($x, $y, $direction) {
         // The passed x & y must be within the boundary
         if(($x > 6 || $x < 0) || ($y > 6 || $y < 0)) {
-            exit("Error: Bike placement must be within the 7 x 7 grid boundaries - i.e. between 0 to 6\n");
+            exit(BikeOnAGrid::ERR_OOB_PLACEMENT);
         }
         $this->currentX = $x;
         $this->currentY = $y;
@@ -37,25 +41,25 @@ class BikeOnAGrid {
     public function forward() {
         if($this->currentDirection === 'NORTH') {
             if($this->currentY == 6) {
-                exit("Error: Bike cannot move any further/out of 7 x 7 grid\n");
+                exit(BikeOnAGrid::ERR_OOB_FORWARD);
             }
             $this->currentY++;
         }
         else if($this->currentDirection === 'EAST') {
             if($this->currentX == 6) {
-                exit("Error: Bike cannot move any further/out of 7 x 7 grid\n");
+                exit(BikeOnAGrid::ERR_OOB_FORWARD);
             }
             $this->currentX++;
         }
         else if($this->currentDirection === 'SOUTH') {
             if($this->currentY == 0) {
-                exit("Error: Bike cannot move any further/out of 7 x 7 grid\n");
+                exit(BikeOnAGrid::ERR_OOB_FORWARD);
             }
             $this->currentY--;
         }
         if($this->currentDirection === 'WEST') {
             if($this->currentX == 0) {
-                exit("Error: Bike cannot move any further/out of 7 x 7 grid\n");
+                exit(BikeOnAGrid::ERR_OOB_FORWARD);
             }
             $this->currentX--;
         }
